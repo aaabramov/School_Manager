067a243c2b5c1c5a392c0949cd9a8b1b2ff6fc13
Abrasha <aabrasha@gmail.com>
2015-09-30 15:41:34 +0300

    + Request class, formatted request parser

--- src/school_manager/server/RequestHandler.java
+++ src/school_manager/server/RequestHandler.java
@@ -5,10 +5,8 @@
  */
 package school_manager.server;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.Socket;
+import school_manager.server.Request.Category;
 
 /**
  *
@@ -16,58 +14,23 @@ import java.net.Socket;
  */
 public class RequestHandler implements Runnable {
 
-    public static void main(String[] args) {
-        parseCategory("users.get");
-    }
+    
+    // regex of requst
+    // CATEGORY:METHOD?(METHOD_SPECIFIC_PARAMS)
     
     private final Socket client;
     private final Server server;
 
     public RequestHandler(Socket client, Server server) {
         this.client = client;
-        this.server =  server;
+        this.server = server;
     }
 
     @Override
     public void run() {
-    /*
-        try {
-            System.out.println("Thread started with name:" + Thread.currentThread().getName());
-            readResponse();
-        } catch (IOException | InterruptedException e) {
-
-        }
-    */
-    }
-    /*
-    private void readResponse() throws IOException, InterruptedException {
-        String userInput;
-        BufferedReader stdIn = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        String request = "";
-        while ((userInput = stdIn.readLine()) != null) {
-            request += userInput;
-        }
-
-        parseRequest(request);
         
+    }
 
-    }
-    */
-    private static void parseCategory(String request){
-        
-        String[] parts = request.split("_");
-        
-        if (request.startsWith("student")){
-            System.out.println("Starts with student");
-        } else if (request.startsWith("teacher")){
-            System.out.println("Starts with teacher");            
-        } else if (request.startsWith("user")){
-            System.out.println("Starts with user");
-        } else if (request.startsWith("parent")){
-            System.out.println("Starts with parent");            
-        } else {
-            System.out.println("with wtf it starts? - " + request);
-        }
-    }
+    
 
 }
