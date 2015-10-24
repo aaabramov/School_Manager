5a10cc601c281b58fa5a2f259cd2653a81279e10
Abrasha <aabrasha@gmail.com>
2015-10-24 10:58:46 +0300

    simple pre-view of Subject object

---
+++ src/school_manager/model/overviews/SubjectOverview.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package school_manager.model.overviews;
+
+/**
+ *
+ * @author abrasha
+ */
+public class SubjectOverview {
+
+    private int id;
+    private String name;
+
+    public SubjectOverview(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+}
