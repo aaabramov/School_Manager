6d049f2ea0805d6e4af7e42b90ce8ee69dda59f5
Abrasha <aabrasha@gmail.com>
2015-10-25 22:35:47 +0200

    + admin pojo, and admins table in db

--- src/school_manager/model/Admin.java
+++ src/school_manager/model/Admin.java
@@ -10,5 +10,30 @@ package school_manager.model;
  * @author bepa
  */
 public class Admin {
-    
+
+    private final int id;
+    private String lastSeen;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLastSeen() {
+        return lastSeen;
+    }
+
+    public void setLastSeen(String lastSeen) {
+        this.lastSeen = lastSeen;
+    }
+
+    public Admin(int id, String lastSeen) {
+        this.id = id;
+        this.lastSeen = lastSeen;
+    }
+
+    @Override
+    public String toString() {
+        return "Admin #" + id;
+    }
+
 }
--- src/school_manager/model/Parent.java
+++ src/school_manager/model/Parent.java
@@ -10,40 +10,47 @@ package school_manager.model;
  * @author abrasha
  */
 public class Parent extends Person {
-    
+
     private String job;
     private int childId;
-    
-    public Parent(Parent.Builder builder){
-        
+
+    public Parent(Parent.Builder builder) {
+
         super(builder);
         this.childId = builder.childId;
         this.job = builder.job;
-        
+
     }
-    public String getJob()
-    {
+
+    public String getJob() {
         return job;
     }
-    
-    public static class Builder extends Person.Builder<Parent.Builder> {
-        
-       private String job;
-       private int childId;
-       
-       public Builder idChild(int childId){
-           this.childId = childId; return this;
-       }
-       
-       public Builder job(String job){
-           this.job = job; return this;
-       }
-       
-       @Override
-       public Parent build(){
-           return (new Parent(this));
-       }
-       
+
+    @Override
+    public String toString() {
+        return getInitials();
     }
-    
+
+    public static class Builder extends Person.Builder<Parent.Builder> {
+
+        private String job;
+        private int childId;
+
+        public Builder idChild(int childId) {
+            this.childId = childId;
+            return this;
+        }
+
+        public Builder job(String job) {
+            this.job = job;
+            return this;
+        }
+
+        @Override
+        public Parent build() {
+            return (new Parent(this));
+        }
+
+    }
+
 }
