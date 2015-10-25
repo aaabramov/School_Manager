de453b1d6439cc8e764783e808552111f75f8779
Abrasha <aabrasha@gmail.com>
2015-10-25 22:38:40 +0200

    now all layout functions are delegated to RootLayoutController, MainApp has only app logic

--- src/school_manager/MainApp.java
+++ src/school_manager/MainApp.java
@@ -1,26 +1,17 @@
 package school_manager;
 
-import java.io.File;
-import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Accordion;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import school_manager.helpers.DatabaseManager;
 import school_manager.model.User;
-import school_manager.view.AdminMenuFragmentController;
-import school_manager.view.LoginFragmentController;
-import school_manager.view.ParentMenuFragmentController;
 import school_manager.view.RootLayoutController;
-import school_manager.view.TeacherMenuFragmentController;
 
 public class MainApp extends Application {
 
@@ -29,21 +20,19 @@ public class MainApp extends Application {
     }
 
     private RootLayoutController rootController;
-    private StackPane contentPane;
-    private StackPane menuPane;
-    private Label statusLabel;
     private User accountInfo;
+    private Logger logger;
 
     @Override
     public void start(Stage stage) throws Exception {
 
+        logger = Logger.getLogger(getClass().getCanonicalName());
+
         Scene scene = initRootLayout();
-        initLogin();
-        //TODO
+        rootController.loadLoginFragment();
 
         stage.setTitle("School Manager");
         stage.getIcons().add(new Image("file:view/res/logl_small.png"));
-//        stage.getIcons().add(new Image(new File("view/res/logl_small.png").toURI().toString()));
         stage.setScene(scene);
         stage.setMinHeight(600);
         stage.setMinWidth(800);
@@ -69,35 +58,12 @@ public class MainApp extends Application {
 
             rootController = loader.getController();
             rootController.setMainApp(this);
-
-            contentPane = rootController.getContentPane();
-            menuPane = rootController.getMenuPane();
-            statusLabel = rootController.getLabelStatus();
             setStatus("RootLoaded");
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            logger.log(Level.SEVERE, "Error initializing Root Layout", e);
         }
 
         return (new Scene(root));
-    }
-
-    private void initLogin() {
-        FXMLLoader loader = new FXMLLoader();
-        BorderPane pane;
-        LoginFragmentController loginController;
-        try {
-
-            loader.setLocation(getClass().getResource("view/LoginFragment.fxml"));
-            pane = (BorderPane) loader.load();
-
-            loginController = loader.getController();
-            loginController.setMainApp(this);
-
-            setContent(pane);
-
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
     }
 
     public MainApp getMainApp() {
@@ -105,36 +71,26 @@ public class MainApp extends Application {
     }
 
     public void setStatus(String status) {
-        if (status != null) {
-            statusLabel.setText(status);
-        } else {
-            statusLabel.setText("Error setting status: null string");
-        }
+        rootController.setStatus(status);
     }
 
     public void setContent(Node content) {
-        if (content != null) {
-            contentPane.getChildren().clear();
-            contentPane.getChildren().add(content);
-        } else {
-            setStatus("Error setting content: null node");
-        }
+        rootController.setContent(content);
+    }
+
+    public void setUserName(String name) {
+        rootController.setUserName(name);
     }
 
     public void setMenu(Node menu) {
-        if (menu != null) {
-            menuPane.getChildren().clear();
-            menuPane.getChildren().add(menu);
-        } else {
-            setStatus("Error setting menu: null node");
-        }
+        rootController.setMenu(menu);
     }
 
     public void logOut() {
         if (accountInfo != null) {
             setAccountInfo(null);
-            menuPane.getChildren().clear();
-            initLogin();
+            rootController.loadLoginFragment();
+            setStatus("User logged out");
         }
     }
 
@@ -142,129 +98,21 @@ public class MainApp extends Application {
 
         if (accountInfo != null) {
 
-            FXMLLoader loader = new FXMLLoader();
-
             this.accountInfo = accountInfo;
-            switch (accountInfo.getAccType()) {
-
-                case STUDENT:
-                /*VBox studentMenuPane;
-
-                 try {
-                 loader.setLocation(getClass().getResource("view/StudentMenuFragment.fxml"));
-                 studentMenuPane = (VBox) loader.load();
-
-                 TeacherMenuFragmentController teacherMenuController = loader.getController();
-                 teacherMenuController.setMainApp(this);
-                 teacherMenuController.setRootLayout(rootController);
-                 teacherMenuController.setTeacher(accountInfo.getId());
-
-                 setMenu(teacherMenuPane);
-                 setStatus("Teacher menu set.");
-                 //TODO teacherMenuController.setTeacher(null);
-                 } catch (IOException e) {
-                 System.out.println(e.getMessage());
-                 setStatus("Error setting teacher menu...");
-                 }
-
-                 break;*/
-                case TEACHER:
-                    VBox teacherMenuPane;
-
-                    try {
-                        loader.setLocation(getClass().getResource("view/TeacherMenuFragment.fxml"));
-                        teacherMenuPane = (VBox) loader.load();
-
-                        TeacherMenuFragmentController teacherMenuController = loader.getController();
-                        teacherMenuController.setMainApp(this);
-                        teacherMenuController.setRootLayout(rootController);
-                        teacherMenuController.setTeacher(accountInfo.getId());
-
-                        setMenu(teacherMenuPane);
-                        setStatus("Teacher menu set.");
-                        //TODO teacherMenuController.setTeacher(null);
-                    } catch (IOException e) {
-                        System.out.println(e.getMessage());
-                        setStatus("Error setting teacher menu...");
-                    }
-
-                    break;
-                case PARENT:
-                    VBox parentMenuPane;
-
-                    try {
-
-                        loader.setLocation(getClass().getResource("view/ParentMenuFragment.fxml"));
-                        parentMenuPane = (VBox) loader.load();
-
-                        ParentMenuFragmentController parentMenuController = loader.getController();
-                        parentMenuController.setMainApp(this);
-                        parentMenuController.setRootLayout(rootController);
-                        parentMenuController.setParent(accountInfo.getId());
-
-                        setMenu(parentMenuPane);
-                        setStatus("Parent menu set.");
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());
-                        setStatus("Error setting parent menu.");
-                    }
-                    break;
-                case ADMIN:
-                    Accordion adminMenuPane;
-
-                    try {
-
-                        loader.setLocation(getClass().getResource("view/AdminMenuFragment.fxml"));
-                        adminMenuPane = (Accordion) loader.load();
-
-                        AdminMenuFragmentController adminMenuController = loader.getController();
-                        adminMenuController.setMainApp(this);
-
-                        setMenu(adminMenuPane);
-                        setStatus("Admin menu set.");
-                    } catch (IOException e) {
-                        System.out.println(e.getMessage());
-                        setStatus("Error settong admin menu");
-                    }
-
-                    break;
-                default:
-
-                    break;
-
-            }
+            rootController.loadMenu(accountInfo.getAccType());
 
         } else {
-
+            setStatus("Error setting user: User is null");
         }
 
     }
 
-    /**
-     *
-     * author bepa
-     */
-    public void LogoClicled() {
-        if (accountInfo != null) {
-            switch (accountInfo.getAccType()) {
-                case STUDENT:
+    public User getAccountInfo() {
+        return accountInfo;
+    }
 
-                    break;
-                case TEACHER:
-
-                    break;
-                case PARENT:
-
-                    break;
-                case ADMIN:
-
-                    break;
-                default:
-                    setContent(null);
-                    break;
-
-            }
-        }
+    public Logger getLogger() {
+        return logger;
     }
 
 }
--- src/school_manager/view/RootLayoutController.java
+++ src/school_manager/view/RootLayoutController.java
@@ -5,15 +5,20 @@
  */
 package school_manager.view;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.beans.value.ObservableValue;
+import java.util.logging.Level;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
+import javafx.scene.Node;
+import javafx.scene.control.Accordion;
 import javafx.scene.control.Label;
 import javafx.scene.control.SplitPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import school_manager.MainApp;
 import school_manager.helpers.MainReferenced;
 import school_manager.model.User;
@@ -25,7 +30,7 @@ import school_manager.model.User;
  */
 public class RootLayoutController implements Initializable, MainReferenced {
 
-    MainApp mainApp;
+    private MainApp mainApp;
 
     @FXML
     private StackPane contentPane;
@@ -33,10 +38,6 @@ public class RootLayoutController implements Initializable, MainReferenced {
     private StackPane menuPane;
     @FXML
     private Label statusLabel;
-    @FXML
-    private Button buttonSearch;
-    @FXML
-    private Button buttonLogout;
     @FXML
     private SplitPane splitPane;
     @FXML
@@ -47,53 +48,54 @@ public class RootLayoutController implements Initializable, MainReferenced {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
-        splitPane.widthProperty().addListener(
-                (ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
-            menuPane.setMaxWidth(newValue.doubleValue() * 0.2);
-            contentPane.setMaxWidth(newValue.doubleValue() * 0.8);
-        });
-        
 
+        splitPane.widthProperty().addListener(
+                (observable, oldValue, newValue) -> {
+
+                    menuPane.resize(newValue.doubleValue() * 0.2, menuPane.getHeight());
+                    menuPane.setMaxWidth(newValue.doubleValue() * 0.2);
+                    contentPane.resize(newValue.doubleValue() * 0.8, contentPane.getHeight());
+                    contentPane.setMaxWidth(newValue.doubleValue() * 0.8);
+                });
     }
 
     @FXML
     public void menuLoginClicked() {
-        
+
     }
-    
+
     @FXML
-    public void btnLogoutClicked(){
+    public void btnLogoutClicked() {
         mainApp.logOut();
     }
-    
+    /*
+     @FXML
+     public void imgLogoClicked(){
+     mainApp.LogoClicked();
+     }*/
+
     @FXML
-    public void imgLogoClicked(){
-        mainApp.LogoClicled();
+    public void menuLoginAsAdminClicked() {
+        mainApp.setAccountInfo(new User(3, 0, User.AccType.ADMIN));
+        setStatus("Forward login as admin");
     }
-    
+
     @FXML
-    public void menuLoginAsAdminClicked(){
-        mainApp.setAccountInfo(new User(0, 0, User.AccType.ADMIN));
-        mainApp.setStatus("Forward login as admin");
+    public void menuLoginAsStudentClicked() {
+        mainApp.setAccountInfo(new User(1, 0, User.AccType.STUDENT));
+        setStatus("Forward login as student");
     }
-    
+
     @FXML
-    public void menuLoginAsStudentClicked(){
-        mainApp.setAccountInfo(new User(0, 0, User.AccType.STUDENT));
-        mainApp.setStatus("Forward login as student");
+    public void menuLoginAsTeacherClicked() {
+        mainApp.setAccountInfo(new User(7, 0, User.AccType.TEACHER));
+        setStatus("Forward login as teacher");
     }
-    
+
     @FXML
-    public void menuLoginAsTeacherClicked(){
-        mainApp.setAccountInfo(new User(0, 0, User.AccType.TEACHER));
-        mainApp.setStatus("Forward login as teacher");
-    }
-    
-    @FXML
-    public void menuLoginAsParentClicked(){
-        mainApp.setAccountInfo(new User(0, 0, User.AccType.PARENT));
-        mainApp.setStatus("Forward login as parent");
+    public void menuLoginAsParentClicked() {
+        mainApp.setAccountInfo(new User(15, 0, User.AccType.PARENT));
+        setStatus("Forward login as parent");
     }
 
     @Override
@@ -115,6 +117,148 @@ public class RootLayoutController implements Initializable, MainReferenced {
 
     public Label getLabelStatus() {
         return statusLabel;
+    }
+
+    public Label getUserInfoLabel() {
+        return nameLabel;
+    }
+
+    public void setStatus(String status) {
+        if (status != null) {
+            statusLabel.setText(status);
+        } else {
+            statusLabel.setText("Error setting status: null string");
+        }
+    }
+
+    public void setContent(Node content) {
+        if (content != null) {
+            contentPane.getChildren().clear();
+            contentPane.getChildren().add(content);
+        } else {
+            setStatus("Error setting content: null node");
+        }
+    }
+
+    public void setUserName(String name) {
+        nameLabel.setText(name);
+    }
+
+    public void setMenu(Node menu) {
+        if (menu != null) {
+            menuPane.getChildren().clear();
+            menuPane.getChildren().add(menu);
+        } else {
+            setStatus("Error setting menu: null node");
+        }
+    }
+
+    
+    public void loadLoginFragment() {
+        menuPane.getChildren().clear();
+        FXMLLoader loader = new FXMLLoader();
+        BorderPane pane;
+        LoginFragmentController loginController;
+        try {
+
+            loader.setLocation(getClass().getResource("LoginFragment.fxml"));
+            pane = (BorderPane) loader.load();
+
+            loginController = loader.getController();
+            loginController.setMainApp(mainApp);
+
+            setContent(pane);
+
+        } catch (IOException e) {
+            mainApp.getLogger().log(Level.SEVERE, "Error initializing Login Layout", e);
+        }
+    }
+    
+    public void loadMenu(User.AccType accType) {
+
+        switch (accType) {
+
+            case STUDENT:
+                setStatus("Student form has not been created yet.");
+                // loadStudentMenu();
+                break;
+            case TEACHER:
+                loadTeacherMenu();
+                break;
+            case PARENT:
+                loadParentMenu();
+                break;
+            case ADMIN:
+                loadAdminMenu();
+                break;
+            case UNKNOWN:
+                setStatus("Unknown type off accaount");
+                break;
+
+        }
+    }
+
+    private void loadStudentMenu() {
+        FXMLLoader loader = new FXMLLoader();
+    }
+
+    private void loadTeacherMenu() {
+        FXMLLoader loader = new FXMLLoader();
+        VBox teacherMenuPane;
+
+        try {
+            loader.setLocation(getClass().getResource("TeacherMenuFragment.fxml"));
+            teacherMenuPane = (VBox) loader.load();
+
+            TeacherMenuFragmentController teacherMenuController = loader.getController();
+            teacherMenuController.setMainApp(mainApp);
+
+            setMenu(teacherMenuPane);
+            setStatus("Teacher menu set.");
+        } catch (IOException e) {
+            mainApp.getLogger().log(Level.SEVERE, "Error setting teacher menu", e);
+            setStatus("Error setting teacher menu...");
+        }
+    }
+
+    private void loadAdminMenu() {
+        FXMLLoader loader = new FXMLLoader();
+        Accordion adminMenuPane;
+
+        try {
+
+            loader.setLocation(getClass().getResource("AdminMenuFragment.fxml"));
+            adminMenuPane = (Accordion) loader.load();
+
+            AdminMenuFragmentController adminMenuController = loader.getController();
+            adminMenuController.setMainApp(mainApp);
+
+            setMenu(adminMenuPane);
+            setStatus("Admin menu set.");
+        } catch (IOException e) {
+            mainApp.getLogger().log(Level.SEVERE, "Error setting admin menu", e);
+            setStatus("Error settong admin menu");
+        }
+    }
+
+    private void loadParentMenu() {
+        FXMLLoader loader = new FXMLLoader();
+        VBox parentMenuPane;
+
+        try {
+
+            loader.setLocation(getClass().getResource("ParentMenuFragment.fxml"));
+            parentMenuPane = (VBox) loader.load();
+
+            ParentMenuFragmentController parentMenuController = loader.getController();
+            parentMenuController.setMainApp(mainApp);
+
+            setMenu(parentMenuPane);
+            setStatus("Parent menu set.");
+        } catch (Exception e) {
+            mainApp.getLogger().log(Level.SEVERE, "Error setting parent menu", e);
+            setStatus("Error setting parent menu.");
+        }
     }
 
 }
